6952deb48a147f99de64f5d06b2f6aa8432faaeb
Ilse Mayela Madrid Gutierrez <immadridg@concredito.com.mx>
2019-02-22 21:06:28 -0700

    first commit

---
+++ src/Components/Login/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './login.css';
+
+class Login extends Component {
+  render() {
+    return <div className="login" />;
+  }
+}
+
+export default Login;
